2b4b810582e03e13bd01e223abda5c03bd2aae0c
Tanzim <tanzim67@gmail.com>
2021-07-27 16:27:33 +0600

    Variable conventions

---
+++ script.js
@@ -0,0 +1,13 @@
+//There cant be space between variables
+
+// var a b="Tanzim"
+//To declare VAR you need to use small letter var you cant use capital letter VAR 
+// VAR a="Tanzim" incorrect
+//Variable cant begin with a number or character 
+// var 3a="Tanzim";
+//You can use an underscore or $ before naming a variable 
+var $a="Tanzim";
+var _b="Web";
+console.log($a,_b);
+//Variables are case sensitive 
+// var A="Tanzim" console.log(a)
