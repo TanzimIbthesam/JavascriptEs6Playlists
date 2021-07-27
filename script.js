ea589ea74822205c4c556e21d5c87c40d6488298
Tanzim <tanzim67@gmail.com>
2021-07-27 13:00:58 +0600

    Variable

---
+++ script.js
@@ -0,0 +1,11 @@
+//How to define a value of a variable in Javascript 
+//var,let and const 
+//declaring a value
+ var a=5;
+ console.log(a);
+ //changing value of variable 
+ a=7;
+ console.log(a);
+//Redeclare a variable 
+var a="Tanzim";
+console.log(a);
