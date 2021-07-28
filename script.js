b5ab9e62e553eefefe5c875e5ec510550020eefa
Tanzim <tanzim67@gmail.com>
2021-07-28 20:51:23 +0600

    Template literal

---
+++ script.js
@@ -0,0 +1,9 @@
+//Template literals 
+//Way by which you can use variable names inside string
+let username="Tanzim101";
+let profession="Web Developer";
+
+//console.log("his name"+username+"His profession is"+profession);
+// Instead of "" use ``
+//${} inside dounble backtips
+console.log(`His name is ${username} and profession is ${profession}`);
