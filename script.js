08762520540b11e131780bed463cdcf872ec0951
Tanzim <tanzim67@gmail.com>
2021-08-01 10:36:56 +0600

    data types

---
+++ script.js
@@ -0,0 +1,15 @@
+//Data types 
+//String-Anything inside " " or '' Number words or characters
+//number-123
+//Boolean-true or false
+//null-The value null represents the intentional absence of any object value
+//undefined-variable not defined
+//Nan-Not a Number
+//Objects-Array,Dates,Literals
+
+
+//how to find out data types in javascript
+
+let username="Tanzim"
+
+console.log(typeof(username));
