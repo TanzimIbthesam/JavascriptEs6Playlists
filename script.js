5977996d0eada889efaed979b667d5e29f29cda1
Tanzim <tanzim67@gmail.com>
2021-08-01 13:36:10 +0600

    String Functions

---
+++ script.js
@@ -0,0 +1,16 @@
+ let username="Tanzim Ibthesam";
+console.log(typeof(username));
+ //Finding out the string length
+console.log(username.length);
+//Finding out the index which is the position of specific alphabet
+console.log(username.indexOf('m'));
+// //Finding out the last index of specific string
+ console.log(username.lastIndexOf('m'));
+// //Finding out what string is present in that specific position
+console.log(username.charAt(3));
+// //Cut values at a starting position and an ending position 
+console.log(username.substr(2,3));
+//Converting all values into uppercase 
+console.log(username.toUpperCase());
+// //Converting all values into lowercase
+console.log(username.toLowerCase());
