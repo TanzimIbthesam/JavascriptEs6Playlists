e11c521af1df491f2776192b5b6b2a0eb6614dad
Tanzim <tanzim67@gmail.com>
2021-08-02 10:25:59 +0600

    StringFunctiontwo

---
+++ script.js
@@ -0,0 +1,10 @@
+let post="     New Post another post "
+console.log(post);
+//Trimmed post
+console.log(post.trim());
+//Slice 
+let posts="Sliced post";
+//takes first position or first index of string and last index of string and it cancels the last index
+console.log(posts.slice(1,3));
+//sub takes first position or first index of string and it goes to total number of places you want to go 
+console.log(posts.substr(1,3));
