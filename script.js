be029bc0e6b0acf9a8029d3a02cdc00831fcdf00
Tanzim <tanzim67@gmail.com>
2021-08-02 15:04:46 +0600

    Array intro

---
+++ script.js
@@ -0,0 +1,17 @@
+// let lists=new Array();
+// console.log(lists);
+// console.log(typeof(lists));
+
+// lists[0]=5
+// lists[1]=6
+// lists[2]="Tanzim"
+// lists[3]=true
+
+// console.log(lists[0]);
+
+let lists=[5,6,"Tanzim",true];
+console.log(lists);
+ console.log(typeof(lists));
+ console.log(lists[2]);
+ console.log(lists[3]);
+
