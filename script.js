4158449100ccd8b1b14ce7fd34d21e0990344079
Tanzim <tanzim67@gmail.com>
2021-08-05 19:07:02 +0600

    Final done

--- script.js
+++ script.js
@@ -1,51 +1,21 @@
 //How to find length of an array
 let posts=["Post One","Post Two","Posts Three","Post Four","Post Five"];
+//push onto array
+// posts.push("Post six")
+// console.log(posts);
+//unshift
+// posts.unshift("Post six")
+// console.log(posts);
+// posts.pop()
+// console.log(posts);
+// //Length of array
 // console.log(posts.length);
-// let lastindex=posts.length-1
-// //Finding out position of the last index
-// console.log(lastindex);
-//What value os there in the last index
-// console.log(posts[lastindex]);
-// posts.push("Post Six")
-// console.log(posts);
-// //Position of last index
-// let lastindex=posts.length-1
-// console.log(lastindex);
-// //Value of last index
-// console.log(posts[lastindex]);
-
-//unshift 
-//position of first index
-// let firstindex=posts[0]
-// //value of the first index
-// console.log(firstindex);
-// posts.unshift("Post Seven")
-// //position of first index
-// let firstindex=posts[0]
-// //value of the first index
-// console.log(firstindex);
-// let lastindex=posts.length-1
-// console.log(lastindex);
-// //finding out the last index
-// console.log(posts[lastindex]);
-//pop 
-// console.log(posts.pop());
-// // console.log(posts);
-
-// let lastindex=posts.length-1
-// //last position of index of array
-// console.log(lastindex);
-// //value of the last index 
-// console.log(posts[lastindex]);
-
+posts.shift()
 console.log(posts);
-console.log(posts.shift());
-// console.log(posts);
-
-let lastindex=posts.length-1
-//last position of index of array
+//Length of array
+console.log(posts.length);
+//position of last index
+let lastindex=posts.length-1;
 console.log(lastindex);
-//value of the last index 
+//Value of the last index
 console.log(posts[lastindex]);
-
-console.log(posts);
