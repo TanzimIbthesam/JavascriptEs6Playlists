626c4798dba3c4b318b3e50310d51ff7817a5b8b
Tanzim <tanzim67@gmail.com>
2021-08-05 19:58:26 +0600

    push,concat,join

---
+++ script.js
@@ -0,0 +1,14 @@
+const fruits=["Apple","Mango",'Orange']
+const fruitstwo=["WaterMelon","Plum","Pear"]
+//join
+// const joinedfruits=fruits.join('-')
+// console.log(joinedfruits);
+// console.log(typeof(joinedfruits));
+
+// let indexoforange=fruits.indexOf("Orange");
+// console.log(indexoforange);
+
+//concat arrays
+let allfruits=fruits.concat(fruitstwo);
+
+console.log(allfruits);
