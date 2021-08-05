8e6d7713d818f070764bc6225cd08895e94f1ad1
Tanzim <tanzim67@gmail.com>
2021-08-05 20:46:42 +0600

    Splice vs splice

---
+++ script.js
@@ -0,0 +1,12 @@
+let numbers=[10,20,30,40,50,60];
+//I want array starting at  index 2 upto the array at index 4
+
+// let slicednumbers=numbers.slice(2,4)
+
+// console.log(slicednumbers);
+let slicednumbers=numbers.slice(2,5)
+
+console.log(slicednumbers);
+//I want array staring at index 2 and go upto 3 spaces including the position of the first selected index or starting index
+let splicenumbers=numbers.splice(2,3)
+console.log(splicenumbers);
