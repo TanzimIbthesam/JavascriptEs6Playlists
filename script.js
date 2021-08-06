f847bacfd6f8e209f8d475def3c9be5527a12f2d
Tanzim <tanzim67@gmail.com>
2021-08-06 15:23:31 +0600

    toString

---
+++ script.js
@@ -0,0 +1,7 @@
+let posts=["Post One","Post Two","Post Three"];
+
+// console.log(posts.toString());
+// let tostringed=posts.toString('/');
+// console.log(tostringed);
+console.log(posts.pop());
+console.log(posts);
