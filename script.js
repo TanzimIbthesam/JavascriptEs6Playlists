020b5fb75a47fb93243eb8ff046d4613fde97dfc
Tanzim <tanzim67@gmail.com>
2021-08-08 17:36:18 +0600

    Numbers

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let a=15; let b=6;
+//addition
+console.log(a+b);
+//subtraction 
+console.log(a-b);
+//multiplication
+// console.log(axb);//wrong 
+console.log(a*b);
+//division 
+console.log(a/b);
+//modulus gives us the remainder
+console.log(a%b);
+
+console.log(14%7);
+//BIDMAS
+
+console.log(2**3/4*4+2-1);
+//Type of 
+console.log(typeof(a));
+
+
+
+
