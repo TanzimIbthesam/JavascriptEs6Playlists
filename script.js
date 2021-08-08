9167f2a7f8be6c17110f6ff5cdde8ed11dda9882
Tanzim <tanzim67@gmail.com>
2021-08-08 20:12:44 +0600

    Abstract and strict equality done

---
+++ script.js
@@ -0,0 +1,8 @@
+let number=25;
+let stringnumber="25";
+console.log(typeof(number));
+console.log(typeof(stringnumber));
+console.log(number==stringnumber);
+console.log(number===stringnumber);
+console.log(number===parseInt(stringnumber));
+console.log(typeof(parseInt(stringnumber)));
