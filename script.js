4c4a3b79603f71779922039917343a2332ec6163
Tanzim <tanzim67@gmail.com>
2021-08-08 20:00:25 +0600

    Boolean

---
+++ script.js
@@ -0,0 +1,24 @@
+let post="Post One";
+
+console.log(post.includes("Post One"));
+console.log(typeof(post.includes("Post One")));
+console.log(post.includes("Post Two"));
+console.log(typeof(post.includes("Post Two")));
+
+let posts=['Post One','Post Two']
+
+console.log(Array.isArray(posts));
+console.log(typeof(Array.isArray(posts)));
+
+
+let numberone=50;
+let numbertwo=55;
+
+console.log(numberone>numbertwo);
+console.log(numberone>=numbertwo);
+console.log(numberone<=numbertwo);
+
+let myname="tanzim"
+let capitalName="Tanzim"
+
+console.log(myname>capitalName);
