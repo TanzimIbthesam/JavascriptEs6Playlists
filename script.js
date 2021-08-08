7f9a48ff5bfb87245d74b68526b96d666b8dbf07
Tanzim <tanzim67@gmail.com>
2021-08-08 19:08:04 +0600

    String methods

---
+++ script.js
@@ -0,0 +1,26 @@
+let numone="55.56"
+
+let numtwo="55"
+console.log(parseInt(numone));
+console.log(parseInt(numtwo));
+let parseone=(parseInt(numone));
+let parsetwo=parseInt()
+console.log(parseone,parsetwo);
+console.log(parseone);
+console.log(parseInt(numone));
+console.log(parseInt(numone));
+console.log(typeof(parseInt(numone)));
+console.log(typeof(parseInt(numtwo)));
+
+console.log(parseFloat(numone));
+console.log(parseFloat(numtwo));
+console.log(typeof(parseFloat(numone)));
+console.log(typeof(parseFloat(numtwo)));
+
+let numbers=56;
+console.log(numbers.toString());
+console.log(typeof(numbers.toString()));
+
+
+
+
