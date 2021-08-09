03d0b1b369b58db811a379d31a3dd3e5787b4ace
Tanzim <tanzim67@gmail.com>
2021-08-09 10:42:10 +0600

    null,undefined,Nan

---
+++ script.js
@@ -0,0 +1,22 @@
+// let a;
+// console.log(a);
+
+// console.log(b);
+
+// let a=null;
+
+// let b=''
+// console.log(typeof(a));
+
+let a="Mark"
+let b=3
+console.log(b/a);
+
+let typeofab=typeof(a/b);
+
+console.log(typeofab);
+
+console.log(true/3);
+console.log(true+true);
+console.log(true+false);
+console.log([]/3);
