178ea0287a69157db9728f05ab082f2a45c8bcec
Tanzim <tanzim67@gmail.com>
2021-08-30 16:12:47 +0600

    done

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let number=25;
 let stringnumber="25";
+
 console.log(typeof(number));
+
 console.log(typeof(stringnumber));
+//comparison using abstract equality ==
 console.log(number==stringnumber);
+//comparison using strict equality ===
 console.log(number===stringnumber);
+//using parseInt 
 console.log(number===parseInt(stringnumber));
-console.log(typeof(parseInt(stringnumber)));
