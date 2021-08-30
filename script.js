bd3e61b8adaecc5ba082ae1699f527a680fffac1
Tanzim <tanzim67@gmail.com>
2021-08-30 19:53:56 +0600

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+//Comparison operators 
+// let a=7;
+// let a=7;
+//Instead
+let a="7";
+console.log(a==7);
+console.log(a===7);
+console.log(a!=7);
+console.log(a!==7);
+
+let b=8;
+console.log(b>9);
+console.log(b<9);
+console.log(b>=8);
+console.log(b<=9);
