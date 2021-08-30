28e86c53e109f4fea20aa2c1ce71a12c94cf954e
Tanzim <tanzim67@gmail.com>
2021-08-30 17:50:20 +0600

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+let a=3;
+//Instead of this
+// a=a+3;
+//We can write 
+a+=3;
+console.log(a);
+//Subtraction
+a-=1;
+console.log(a);
+//Multiplication
+a*=4;
+console.log(a);
+//Modulus 
+a%=6;
+console.log(a);
+//To the power
+a**=2;
+console.log(a);
+
