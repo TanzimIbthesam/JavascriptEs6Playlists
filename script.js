92ce3aecafe5c2a6672357355f8c2944d0fe8dfe
Tanzim <tanzim67@gmail.com>
2021-09-20 12:23:54 +0600

    For Loop

---
+++ script.js
@@ -0,0 +1,18 @@
+//Suppose we want to print from 1-10
+
+for(i=1;i<11;i++){
+    console.log(i);
+}
+//Looping through an Array of fruits
+let allFruits=["Mango","Banana","Orange","Peach","Kiwi"];
+
+console.log(allFruits.length);
+
+//iterate through array 
+for(i=0;i<allFruits.length;i++){
+    console.log(allFruits[i]);
+}
+//From 1 to 3
+for(i=1;i<3;i++){
+    console.log(allFruits[i]);
+}
