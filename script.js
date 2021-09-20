644e2a346589f0a7f4f59ec33ba568fea2d0fba0
Tanzim <tanzim67@gmail.com>
2021-09-20 17:28:15 +0600

    switch case

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let fruit="Peach";
+
+switch(fruit){
+    case "Apple":
+   console.log("The price is 2USD");
+   break
+    case "Papaya":
+   console.log("The price is 3USD");
+   break
+    case "Orange":
+   console.log("The price is 4USD");
+   break
+   case "PineApple":
+    console.log("The price is 1USD");
+    break
+    default:
+    console.log("Your search is invalid");
+}
