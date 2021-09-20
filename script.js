b0246d324deccbe43c5a0ae0af2ad3079f2db584
Tanzim <tanzim67@gmail.com>
2021-09-20 16:27:48 +0600

    If-else-else-if

---
+++ script.js
@@ -0,0 +1,23 @@
+// let age=16;
+
+// if(age>=18){
+//     console.log("You are an adult");
+// }else if(age>=16 && age<=17){
+//     console.log("You are young");
+// }else{
+//     console.log("You are a children");
+// }
+//Marks 
+
+let marks=30;
+if(marks>=80 && marks<=100){
+    console.log("You got A+");
+} else if(marks>=60 && marks<=79){
+    console.log("You got A");
+}else if(marks>=50 && marks<=59){
+      console.log("You got B");
+}else if(marks>=40 && marks<=49){
+    console.log("You get C");
+}else{
+    console.log("You get F");
+}
