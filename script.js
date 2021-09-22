bfd094bba3042f37cbe76b22836d521f61521bd4
Tanzim <tanzim67@gmail.com>
2021-09-22 21:21:54 +0600

    Done

---
+++ script.js
@@ -0,0 +1,15 @@
+//Write a function 
+//Declare the parameters
+// function profile(name,age){
+//     console.log(`My name is ${name} and age is ${age}`);
+
+// }
+// //Pass the arguments
+// profile("Tanzim",25);
+//Not much recommended
+function profile(name="Tanzim",age=25){
+    console.log(`My name is ${name} and age is ${age}`);
+
+}
+//Pass the arguments
+profile();
