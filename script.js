11b5c63823b7e14d7c7a96623ce98ec5c0f982df
Tanzim <tanzim67@gmail.com>
2021-09-22 23:28:43 +0600

    Done

---
+++ script.js
@@ -0,0 +1,15 @@
+//
+// const allnames=(name,age)=>{
+//     console.log(`My name is ${name} and age is ${age}`);
+// }
+
+// allnames("Tanzim",25);
+
+// const add=(a)=>{
+//     return a**a;
+// }
+//We can write in Es6 if you only have one parameter
+const add=a=>a*a;
+
+const alladd=add(2)
+console.log(alladd);
