034fffe7976cf574f599735b82fd96db6dcc980d
Tanzim <tanzim67@gmail.com>
2021-09-23 19:05:15 +0600

    Done

---
+++ script.js
@@ -0,0 +1,24 @@
+//function declaration 
+// myprofile();
+// function myprofile(){
+//     console.log("My profile");
+// }
+// myprofile();
+
+
+//function expression 
+
+//Keyword function
+// myprofile();
+// let myprofile=function(){
+//     console.log("My profile");
+// }
+
+// myprofile();
+
+//Fat arrow function 
+// myprofile();
+let myprofile=()=>{
+    console.log("MY profile");
+}
+myprofile()
