cb52fa75a2110045d9bdf76d41d456376820da49
Tanzim <tanzim67@gmail.com>
2021-09-23 16:40:08 +0600

    FunctionnMethods

---
+++ script.js
@@ -0,0 +1,13 @@
+//function 
+//user defined
+function newName(){
+    console.log("New Name");
+}
+newName();
+//method 
+let myname="Tanzim";
+
+let uppercasename=myname.toUpperCase()
+
+console.log(uppercasename);
+
