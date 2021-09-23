dcbd468299b3b4e5fed6fa02da5bb2e3e93bab81
Tanzim <tanzim67@gmail.com>
2021-09-23 15:29:58 +0600

    forEachmethod

---
+++ script.js
@@ -0,0 +1,22 @@
+//Loop through an array 
+
+let fruits=["Peach",'Orange',"Kiwi","Pear","Apple"];
+
+//find length of array 
+
+// let fruitslength=fruits.length;
+
+// for (let i = 0; i < fruits.length; i++) {
+//     const element = fruits[i];
+//     console.log(element);
+    
+// }
+//forEach method 
+//Es5
+// fruits.forEach(function(fruit){
+//     console.log(fruit);
+// });
+
+//es6 way
+
+fruits.forEach(fruit=>console.log(fruit));
