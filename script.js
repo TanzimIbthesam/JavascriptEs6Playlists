017ee33e2d44df42d3b1bc17c5de2b9971ac1862
Tanzim <tanzim67@gmail.com>
2021-09-24 16:42:17 +0600

    Done

---
+++ script.js
@@ -0,0 +1,11 @@
+let posts={
+    title:"My First Post",description:"Description of my first post",isPublished:false
+}
+
+console.log(posts);
+
+//Typeof 
+
+console.log(typeof(posts));
+
+console.log(posts.title,posts.description,posts.isPublished);
