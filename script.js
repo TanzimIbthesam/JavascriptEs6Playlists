4801dfcea1b1cc9890832451d6ab60c40acc374a
Tanzim <tanzim67@gmail.com>
2021-09-24 18:38:50 +0600

    Done

---
+++ script.js
@@ -0,0 +1,18 @@
+let posts={
+    title:"First Post",description:"Get description of first post",
+    // getAllPosts:function(){
+    //     console.log("Get all the posts");  
+    // }
+
+
+    getAllPosts(){
+        console.log("Get all the posts");  
+    }
+
+    
+}
+
+//invoke the method
+
+// console.log(posts.getAllPosts());
+posts.getAllPosts();
