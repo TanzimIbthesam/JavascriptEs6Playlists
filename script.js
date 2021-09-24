55d7f7d3499e511e5e5fe68cdf97b3545732498f
Tanzim <tanzim67@gmail.com>
2021-09-24 19:55:30 +0600

    Done

---
+++ script.js
@@ -0,0 +1,16 @@
+let posts=[
+    {id:1,title:"First Post",description:"Description"},
+    {id:2,title:"First Post",description:"Description"},
+    {id:3,title:"First Post",description:"Description"},
+    {id:4,title:"First Post",description:"Description"},
+]
+// let fruits=["Apple","Banana","Orange"];
+// console.log(typeof(fruits));
+// console.log(posts);
+
+// console.log(typeof(posts));
+
+posts.forEach(post=>{
+    console.log(post.title)
+
+});
