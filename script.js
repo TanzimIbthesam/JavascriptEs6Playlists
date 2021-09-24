464f358f4555f2451a698e6a85afb15ba0b15f18
Tanzim <tanzim67@gmail.com>
2021-09-25 00:19:48 +0600

    Done

---
+++ script.js
@@ -0,0 +1,12 @@
+let num=4.55;
+//Math round
+// Math.round(num)
+// console.log(Math.round(num));
+//Math ceil 
+
+// console.log(Math.ceil(num));
+//Math floor
+
+//  console.log(Math.floor(num));
+
+console.log(Math.floor(Math.random()*25));
