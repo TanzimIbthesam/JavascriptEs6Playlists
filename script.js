0859c3176e7a7513d64afaaa8a8b67cb61f77ffc
Tanzim <tanzim67@gmail.com>
2021-09-25 12:07:04 +0600

    Done

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let num=4.55;
 //Math round
-// Math.round(num)
-// console.log(Math.round(num));
-//Math ceil 
+let num=4.75;
+let numtwo=4.25;
+ console.log(Math.round(num));
+ console.log(Math.round(numtwo));
 
-// console.log(Math.ceil(num));
-//Math floor
+console.log(Math.ceil(num));
+console.log(Math.ceil(numtwo));
 
-//  console.log(Math.floor(num));
+console.log(Math.floor(num));
+console.log(Math.floor(numtwo));
 
-console.log(Math.floor(Math.random()*25));
+console.log(Math.floor(Math.random()*100));
+let thename="Sumudu"
+console.log(2/"sumudu");
